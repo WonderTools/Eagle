0c0b9f4db3b2f869b2c4b228d8ef4cca3deead65
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-05-11 11:14:10 +0530

    Initial start of eagle

---
+++ Eagle/Eagle.Web/Controllers/FeaturesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Eagle.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eagle.Controllers
+{
+    [Route("api/features")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<FeatureIdAndNames>> GetFeatureNames()
+        {
+            return new List<FeatureIdAndNames>();
+        }
+    }
+}
---
+++ Eagle/Eagle.Web/Models/FeatureIdAndNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eagle.Models
+{
+    public class FeatureIdAndNames
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
