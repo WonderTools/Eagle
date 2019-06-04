feb70818941edb56d0298f572dd2154f4e479031
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-06-04 11:20:52 +0530

    Refactored

--- Eagle/Eagle/Initializer.cs
+++ Eagle/Eagle/Initializer.cs
@@ -18,97 +18,10 @@ namespace Eagle
                 {
                     var xml = runner.Explore(TestFilter.Empty);
                     var json = xml.ToJson();
-                    return GetHardCodedTestSuite(json);
+                    var nUnitJsonParser = new NUnitJsonParser();
+                    return nUnitJsonParser.GetTestSuiteFromDiscoveryJson(json);
                 }
             }
-        }
-
-        public static TestSuite GetHardCodedTestSuite(string json)
-        {
-            //TBD: This has to be refactored
-            return new TestSuite()
-            {
-                Name = "Feature.Infrastructure.dll",
-                FullName = "Feature.Infrastructure.dll",
-                TestSuites = new List<TestSuite>()
-                {
-                    new TestSuite()
-                    {
-                        Name = "Feature",
-                        FullName = "Feature",
-                        TestSuites = new List<TestSuite>()
-                        {
-                            new TestSuite()
-                            {
-                                Name = "Infrastructure",
-                                FullName = "Feature.Infrastructure",
-                                TestSuites = new List<TestSuite>()
-                                {
-                                    new TestSuite()
-                                    {
-                                        Name = "EagleFeature",
-                                        FullName = "Feature.Infrastructure.EagleFeature",
-                                        TestCases = new List<TestCase>()
-                                        {
-                                            new TestCase()
-                                            {
-                                                Name = "AddTwoNumbers",
-                                                FullName = "Feature.Infrastructure.EagleFeature.AddTwoNumbers",
-                                            },
-                                            new TestCase()
-                                            {
-                                                Name = "SubtractTwoNumbers",
-                                                FullName = "Feature.Infrastructure.EagleFeature.SubtractTwoNumbers",
-                                            },
-                                        }
-                                    },
-                                    new TestSuite()
-                                    {
-                                        Name = "TestClass",
-                                        FullName = "Feature.Infrastructure.TestClass",
-                                        TestCases = new List<TestCase>()
-                                        {
-                                            new TestCase()
-                                            {
-                                                Name = "TestMethod",
-                                                FullName = "Feature.Infrastructure.TestClass.TestMethod",
-                                            },
-                                        }
-                                    },
-                                    new TestSuite()
-                                    {
-                                        Name = "TestMyClass",
-                                        FullName = "Feature.Infrastructure.TestMyClass",
-                                        TestCases = new List<TestCase>()
-                                        {
-                                            new TestCase()
-                                            {
-                                                Name = "MyTest(1)",
-                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(1)",
-                                            },
-                                            new TestCase()
-                                            {
-                                                Name = "MyTest(2)",
-                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(2)",
-                                            },
-                                            new TestCase()
-                                            {
-                                                Name = "MyTest(3)",
-                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(3)",
-                                            },
-                                            new TestCase()
-                                            {
-                                                Name = "MyTest(4)",
-                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(4)",
-                                            },
-                                        }
-                                    },
-                                }
-                            }
-                        }
-                    }
-                }
-            };
         }
 
         private List<(string FullName, string Id)> GetFullNameAndIds(TestSuite suite)
---
+++ Eagle/Eagle/NUnitJsonParser.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace Eagle
+{
+    public class NUnitJsonParser
+    {
+        public TestSuite GetTestSuiteFromDiscoveryJson(string json)
+        {
+            //TBD: This has to be refactored
+            return new TestSuite()
+            {
+                Name = "Feature.Infrastructure.dll",
+                FullName = "Feature.Infrastructure.dll",
+                TestSuites = new List<TestSuite>()
+                {
+                    new TestSuite()
+                    {
+                        Name = "Feature",
+                        FullName = "Feature",
+                        TestSuites = new List<TestSuite>()
+                        {
+                            new TestSuite()
+                            {
+                                Name = "Infrastructure",
+                                FullName = "Feature.Infrastructure",
+                                TestSuites = new List<TestSuite>()
+                                {
+                                    new TestSuite()
+                                    {
+                                        Name = "EagleFeature",
+                                        FullName = "Feature.Infrastructure.EagleFeature",
+                                        TestCases = new List<TestCase>()
+                                        {
+                                            new TestCase()
+                                            {
+                                                Name = "AddTwoNumbers",
+                                                FullName = "Feature.Infrastructure.EagleFeature.AddTwoNumbers",
+                                            },
+                                            new TestCase()
+                                            {
+                                                Name = "SubtractTwoNumbers",
+                                                FullName = "Feature.Infrastructure.EagleFeature.SubtractTwoNumbers",
+                                            },
+                                        }
+                                    },
+                                    new TestSuite()
+                                    {
+                                        Name = "TestClass",
+                                        FullName = "Feature.Infrastructure.TestClass",
+                                        TestCases = new List<TestCase>()
+                                        {
+                                            new TestCase()
+                                            {
+                                                Name = "TestMethod",
+                                                FullName = "Feature.Infrastructure.TestClass.TestMethod",
+                                            },
+                                        }
+                                    },
+                                    new TestSuite()
+                                    {
+                                        Name = "TestMyClass",
+                                        FullName = "Feature.Infrastructure.TestMyClass",
+                                        TestCases = new List<TestCase>()
+                                        {
+                                            new TestCase()
+                                            {
+                                                Name = "MyTest(1)",
+                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(1)",
+                                            },
+                                            new TestCase()
+                                            {
+                                                Name = "MyTest(2)",
+                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(2)",
+                                            },
+                                            new TestCase()
+                                            {
+                                                Name = "MyTest(3)",
+                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(3)",
+                                            },
+                                            new TestCase()
+                                            {
+                                                Name = "MyTest(4)",
+                                                FullName = "Feature.Infrastructure.TestMyClass.MyTest(4)",
+                                            },
+                                        }
+                                    },
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
