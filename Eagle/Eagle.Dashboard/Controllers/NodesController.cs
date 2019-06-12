2f8d5a47a90fcd171cbb19914db57a76add6d751
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-06-12 20:42:27 +0530

    Separation of controller

---
+++ Eagle/Eagle.Dashboard/Controllers/NodesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eagle.Dashboard.Controllers
+{
+    [Route("api/nodes")]
+    [ApiController]
+    public class NodesController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult CreateNode()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Eagle/Eagle.Dashboard/Controllers/TestsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eagle.Dashboard.Controllers
+{
+    [Route("api/tests")]
+    [ApiController]
+    public class TestsController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetTests()
+        {
+            return Ok();
+        }
+
+        [HttpPost("schedule")]
+        public ActionResult ScheduleTests([FromBody]string id)
+        {
+            return Ok();
+        }
+
+
+    }
+}
