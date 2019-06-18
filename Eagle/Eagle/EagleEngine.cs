3ba2ecbf2f40a3add4ca779214236ef44d3d4587
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-06-18 15:34:03 +0530

    Clean Up

--- Eagle/Eagle/EagleEngine.cs
+++ Eagle/Eagle/EagleEngine.cs
@@ -13,31 +13,13 @@ namespace Eagle
     public class EagleEngine
     {
         private readonly IResultHandler _handler;
-        private readonly ITestQueue _testQueue;
         private TestRunner _testRunner;
         private List<TestSuite> _testSuites;
         private Dictionary<string,(TestPackage TestPackage, string FullName)> _idToSchedulingParametersMap;
         
         public EagleEngine(IResultHandler handler)
         {
-            _testQueue = new ThreadSafeQueue();
             _handler = handler;
-        }
-
-        public async Task Process()
-        {
-            await _testRunner.Process();
-        }
-
-        public async Task ExecuteOne()
-        {
-            await _testRunner.ExecuteOne();
-        }
-
-        public string ScheduleTest(string id)
-        {
-            if (!_idToSchedulingParametersMap.ContainsKey(id)) throw new Exception("The Id is not found");
-            return _testQueue.AddToQueue(id);
         }
 
         public async Task<MyResult> ExecuteTest(string id, string nodeName, string requestId, string uri)
@@ -57,20 +39,6 @@ namespace Eagle
             return executeTest;
         }
 
-
-
-        public List<ScheduledTest> GetScheduledFeatures()
-        {
-            var queueElements = _testQueue.GetQueueElements();
-            return queueElements.Select(x => new ScheduledTest()
-            {
-                //TBD Refactor to use auto mapper
-                FullName = _idToSchedulingParametersMap[x.Id].FullName,
-                Id = x.Id,
-                SerialNumber = x.SerialNumber,
-            }).ToList();
-        }
-
         public List<TestSuite> GetDiscoveredTestSuites()
         {
             return _testSuites;
@@ -82,7 +50,7 @@ namespace Eagle
             var packageToSuiteMap = initializer.GetTestPackageToTestSuiteMap(testAssembliesLocationHolder);
             _testSuites = packageToSuiteMap.Values.ToList();
             _idToSchedulingParametersMap = initializer.GetIdToSchedulingParametersMap(packageToSuiteMap);
-            _testRunner = new TestRunner(_idToSchedulingParametersMap, _testQueue);
+            _testRunner = new TestRunner(_idToSchedulingParametersMap);
         }
     }
 
--- Eagle/Eagle/TestRunner.cs
+++ Eagle/Eagle/TestRunner.cs
@@ -11,69 +11,11 @@ namespace Eagle
     public class TestRunner
     {
         private readonly Dictionary<string, (TestPackage TestPackage, string FullName)> _idToSchedulingParametersMap;
-        private readonly ITestQueue _testQueue;
         private ScheduledTestInternal _runningTest;
-        private object _lockable = new object();
 
-        public TestRunner(Dictionary<string, (TestPackage TestPackage, string FullName)> idToSchedulingParametersMap, 
-            ITestQueue testQueue)
+        public TestRunner(Dictionary<string, (TestPackage TestPackage, string FullName)> idToSchedulingParametersMap)
         {
             _idToSchedulingParametersMap = idToSchedulingParametersMap;
-            _testQueue = testQueue;
-        }
-
-        public async Task Process()
-        {
-            
-            lock (_lockable)
-            {
-                if (_runningTest != null) return;
-            }
-
-            lock (_lockable)
-            {
-                _runningTest = _testQueue.RemoveTopOfQueue();
-                if (_runningTest == null) return;
-            }
-
-            await Task.Run(async () =>
-            {
-                await RunTestCase(_runningTest.Id);
-                lock (_lockable)
-                {
-                    _runningTest = null;
-                }
-            });
-        }
-
-        public async Task ExecuteOne()
-        {
-            var top = _testQueue.RemoveTopOfQueue();
-            if(top == null) return;
-            await RunTestCase(top.Id);
-        }
-
-        private async Task RunTestCase(string id)
-        {
-            var schedulingParameters = _idToSchedulingParametersMap[id];
-            await RunTestCase(schedulingParameters.TestPackage, schedulingParameters.FullName);
-        }
-
-        private async Task RunTestCase(TestPackage testPackage, string name)
-        {
-            using (var engine = TestEngineActivator.CreateInstance())
-            {
-                using (var runner = engine.GetRunner(testPackage))
-                {
-                    //TBD: Check if the scheduling can be done with internal id
-                    var filterService = engine.Services.GetService<ITestFilterService>();
-                    var builder = filterService.GetTestFilterBuilder();
-                    builder.AddTest(name);
-                    var filter = builder.GetFilter();
-                    var x =   runner.Run(new TestEventListener(), filter);
-                    await HandleResults(x.ToJson());
-                }
-            }
         }
 
         public async Task<List<TestResult>> RunTestCaseNew(string id)
@@ -129,14 +71,6 @@ namespace Eagle
                 Result = x.Result,
 
             }).ToList();
-        }
-
-        private async Task HandleResults(string json)
-        {
-            Console.WriteLine("Handling result" + json);
-            var jsonParser = new NUnitJsonParser();
-            var runTestSuite= jsonParser.GetTestSuiteFromResultJson(json);
-            var testCases= GetTestCases(runTestSuite);
         }
 
         private List<ResultTestCase> GetTestCases(ResultTestSuite testSuite)
