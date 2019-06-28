f24d1a62e91b0e29b5dd88e1a5e5223fd3a9b42f
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-06-28 22:30:02 +0530

    Implemented middle ware for tests

--- Eagle/Eagle.TestNode2/Startup.cs
+++ Eagle/Eagle.TestNode2/Startup.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.Swagger;
+using WonderTools.Eagle;
+using WonderTools.Eagle.Communication.Contract;
 
 namespace Eagle.TestNode2
 {
@@ -61,32 +67,68 @@ namespace Eagle.TestNode2
 
             app.Use(async (context, next) =>
             {
-                var path = context.Request.Path.ToString().ToLower();
-                if (path != "/api/tests/execute") await next.Invoke();
-                if (context.Request.Method.ToLower() != "post") await next.Invoke();
-                //using (var reader = new StreamReader(
-                //       context.Request.Body,
-                //       encoding: Encoding.UTF8,
-                //       detectEncodingFromByteOrderMarks: false,
-                //       bufferSize: bufferSize,
-                //       leaveOpen: true))
+                if (!IsEagleExecute(context))
                 {
-                    var body = await reader.ReadToEndAsync();
-                    // Do some processing with body…
-
-                    // Reset the request body stream position so the next middleware can read it
-                    context.Request.Body.Position = 0;
+                    await next.Invoke();
+                    return;
                 }
 
-                var i = 0;
-
-
-                
+                await HandleEagleRequest(context);
             });
 
 
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private static async Task HandleEagleRequest(HttpContext context)
+        {
+            var bodyAsText = await GetBody(context);
+            TestTrigger testTrigger = JsonConvert.DeserializeObject<TestTrigger>(bodyAsText);
+
+            var eagleEngine = new EagleEngine(typeof(AzureResponseCodeTests));
+            var testSuites = eagleEngine.GetDiscoveredTestSuites();
+            IResultHandler handler = new HttpRequestResultHandler(testSuites, testTrigger.NodeName, testTrigger.RequestId,
+                testTrigger.CallBackUrl);
+            var result = await eagleEngine.ExecuteTest(handler, testTrigger.Id);
+
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int) HttpStatusCode.OK;
+            var report = new TestReport()
+            {
+                TestResults = result,
+                TestSuites = testSuites,
+                NodeName = testTrigger.NodeName,
+                RequestId = testTrigger.RequestId,
+            };
+            await response.WriteAsync(JsonConvert.SerializeObject(report));
+        }
+
+        private bool IsEagleExecute(HttpContext context)
+        {
+            var path = context.Request.Path.ToString().ToLower();
+            if (path != "/api/tests/execute") return false;
+            if (context.Request.Method.ToLower() != "post") return false;
+            return true;
+        }
+
+
+        private static async Task<string> GetBody(HttpContext context)
+        {
+            var request = context.Request;
+            var body = request.Body;
+            //This line allows us to set the reader for the request back at the beginning of its stream.
+            request.EnableRewind();
+            //We now need to read the request stream.  First, we create a new byte[] with the same length as the request stream...
+            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
+            //...Then we copy the entire request stream into the new buffer.
+            await request.Body.ReadAsync(buffer, 0, buffer.Length);
+            //We convert the byte[] into a string using UTF8 encoding...
+            var bodyAsText = Encoding.UTF8.GetString(buffer);
+            //..and finally, assign the read body back to the request body, which is allowed because of EnableRewind()
+            request.Body = body;
+            return bodyAsText;
         }
     }
 }
