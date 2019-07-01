bb4de298e6beadc4c023eec3594a6359f54440f7
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-07-01 15:47:58 +0530

    Clean code

--- Eagle/FunctionApp1/Function1.cs
+++ Eagle/FunctionApp1/Function1.cs
@@ -24,17 +24,6 @@ namespace FunctionApp1
             return await RequestHandler.HandleRequest(req, typeof(TestMyClass));
 
 
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
